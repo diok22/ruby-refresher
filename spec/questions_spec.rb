a5c11442463a7a090ca587a86f6eb129b79a827d
Dionysis <Dionysis.k22@gmail.com>
2016-11-13 11:34:58 +0000

    removed rspec configuration from question_spec rb

--- spec/questions_spec.rb
+++ spec/questions_spec.rb
@@ -1,10 +1,10 @@
 require 'rspec'
 require 'questions'
 
-RSpec.configure do |config|
-  config.color = true
-  config.formatter = :documentation
-end
+# RSpec.configure do |config|
+#   config.color = true
+#   config.formatter = :documentation
+# end
 
 describe 'the Friday test :)' do
 
